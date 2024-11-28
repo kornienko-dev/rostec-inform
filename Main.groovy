5af53f7b18559d13c08c3995cc6d497c42237476
Ivan Kornienko <kornienko.dev@gmail.com>
2024-11-28 14:36:50 +0500

    Update Main.groovy

--- Main.groovy
+++ Main.groovy
@@ -1,11 +1,9 @@
-class Main {
-    static void main(String[] args) {
-        println(transform([1, 3, 4, 5, 1, 5, 4] as Integer[]))
-    }
+static void main(String[] args) {
+    println(transform([1, 3, 4, 5, 1, 5, 4] as Integer[]))
+}
 
-    static <T> Map<T, Long> transform(T[] array) {
-        array.toList()
-             .groupBy { it }
-             .collectEntries { k, v -> [(k): v.size().toLong()] }
-    }
+static <T> Map<T, Long> transform(T[] array) {
+    array.toList()
+            .groupBy { it }
+            .collectEntries { k, v -> [(k): v.size().toLong()] }
 }
